fd0785fe5b622378a7a29c887d2cf83535355821
2STO <76927986+2STO@users.noreply.github.com>
2021-02-03 14:56:38 +0800

    Add files via upload

---
+++ src/main/java/com/STO/breadapp/Main.java
@@ -0,0 +1,13 @@
+package com.STO.breadapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreadappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreadappApplication.class, args);
+	}
+
+}
